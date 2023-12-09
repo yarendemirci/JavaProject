eb7034d53708393a89d6a1d94eec113246c95f3f
Yaren <yaren_demirci@outlook.com>
2023-12-09 23:34:02 +0300

    Dairede alan eklendi

--- src/DaireAlan.java
+++ src/DaireAlan.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class DaireAlan {
     public static void main(String[] args) {
-        double pi=3.14;
-        double r,a,alan;
-        Scanner input=new Scanner(System.in);
+        double pi = 3.14;
+        double r, a, alan;
+        Scanner input = new Scanner(System.in);
         System.out.println("Dairenin yarıçapını giriniz:");
-        r=input.nextDouble();
+        r = input.nextDouble();
         System.out.println("Dairenin ölçüsünü giriniz: ");
-        a=input.nextDouble();
-        alan=(pi*r*r*a)/360;
-        System.out.println("dairenin alanı: " +alan);
+        a = input.nextDouble();
+        alan = (pi * r * r * a) / 360;
+        System.out.println("dairenin alanı: " + alan);
 
     }
 }
